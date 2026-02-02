2ba8dc9f6dc0e287d9e56604c23ec3c7f3a5a85e
BY-ee <BY-ee@naver.com>
2026-02-02 17:36:12 +0900

    feat: header 스크롤 방향에 따라 숨김/표시 처리

--- images/common.js
+++ images/common.js
@@ -1,1 +1,17 @@
 // common.js
+let lastScrollY = window.scrollY;
+
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    const currentScrollY = window.scrollY;
+
+    if (!header) return;
+
+    if (currentScrollY > lastScrollY) {
+        header.style.top = '-83px';
+    } else {
+        header.style.top = '0';
+    }
+
+    lastScrollY = currentScrollY;
+});
